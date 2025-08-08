7e7f33d9c941b6f59ce1f9b0a3c4c4f8594dad3b
Drashtee Parmar <drashteeparmar@gmail.com>
2025-08-08 14:06:05 -0400

    remove unnecessary changes

--- app.py
+++ app.py
@@ -150,4 +150,6 @@ def teaching_assistant_chat():
 
 # Run App
 if __name__ == "__main__":
-    app.run(debug=True)
+    # app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
